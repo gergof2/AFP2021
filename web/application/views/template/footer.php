5942b5828cea93c549dfeda8fa78cd0089e70824
Szalóki Dávid <Szunyix@gmail.com>
2021-03-17 17:09:19 +0100

    Frontend footer -> complete version

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -1,19 +1,43 @@
 <footer>
-	<div class="footer2">
+	<div class="footer2" style="">
 		<div class="inner_footer">
 			<div class="logo_container">
-				<img src="/picture/asd.jpg">				
+				<img src="/assets/vendor/images/large.png">	<!-- picture -->	
 			</div>
 
 			<div class="footer_third">
-				
+				<h1>Need Help?</h1>
+				<a href="#">Help page</a>
 			</div>
+
+			<div class="footer_third">
+				<h1>More</h1>
+				<a href="#">Brouchures</a>
+				<a href="#">Donate</a>
+				<a href="#">Grovernace</a>
+				<a href="#">Impact Report</a>
+			</div>
+
+			<div class="footer_third">
+				<h1>Follow Us</h1>
+				<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+
+			</div>
+			<div class="footer_third">
+				<span>
+					© Horvát Dániel <br>
+					© Kerepesi Gergő <br>
+					© Seres Péter <br>
+					© Szalóki Dávid 
+				</span>
+
+			</div>
+
 		</div>
 	</div>
-<p class="text" style="">© Horvát Dániel <br>
-© Kerepesi Gergő <br>
-© Seres Péter <br>
-© Szalóki Dávid 
+
  </p>
 </footer>
 
