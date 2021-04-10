3a600527b8ed2c5985af655432f1d627c146f91c
Szalóki Dávid <Szunyix@gmail.com>
2021-04-10 09:20:23 +0200

    Login And Register Style Complete

--- web/application/views/home/login.php
+++ web/application/views/home/login.php
@@ -1,1 +1,18 @@
-asdasfasdasd
+<div class="title mycolor">
+	<h2>Login</h2>
+</div>
+<div class="mycolor center">
+	<form action="api/login" method="post">
+		<div >
+			<input class="input" type="text" name="username" required placeholder="Username">
+		</div>
+		<div>
+			<input class="input" type="text" name="password" required placeholder="Password">
+		</div>
+
+		<button class="button mycolor" type="submit" name="login_user">Log In</button>
+
+		<p>Not a user? <a href="/registration"><b>Register Here</b></a></p>
+		
+	</form>
+</div>
