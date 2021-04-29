b70cce6c28e12054e9004130b664ff53da4e09a2
seresp <71933391+SeresP99@users.noreply.github.com>
2021-04-30 01:58:03 +0200

    Created LoginWindow.cpp Constructor

--- C++ Client/Timber Desktop/Timber Desktop/LoginWindow.cpp
+++ C++ Client/Timber Desktop/Timber Desktop/LoginWindow.cpp
@@ -8,3 +8,12 @@ wxBEGIN_EVENT_TABLE(LoginWindow, wxFrame)
 EVT_BUTTON(10001, loginButtonClick)
 EVT_BUTTON(10002, registerButtonClick)
 wxEND_EVENT_TABLE()
+
+LoginWindow::LoginWindow() : wxFrame(nullptr, wxID_ANY, "Timber Desktop", wxPoint(30, 30), wxSize(310, 400))
+{
+	this->SetBackgroundColour(wxColor(*wxWHITE));
+	loginBtn = new wxButton(this, 10001, "Log In", wxPoint(93, 320), wxSize(100, 25));
+	registerBtn = new wxButton(this, 10001, "Log In", wxPoint(93, 320), wxSize(100, 25));
+	usernameTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 260), wxSize(200, 25));
+	passwordTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 290), wxSize(200, 25));
+}
