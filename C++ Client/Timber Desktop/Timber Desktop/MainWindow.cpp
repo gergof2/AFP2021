ec7058e950527cd53a75151d749b19eea9d98bb6
seresp <71933391+SeresP99@users.noreply.github.com>
2021-04-30 13:37:01 +0200

    Finished MainWindow GUI Controls

--- C++ Client/Timber Desktop/Timber Desktop/MainWindow.cpp
+++ C++ Client/Timber Desktop/Timber Desktop/MainWindow.cpp
@@ -3,6 +3,10 @@
 MainWindow::MainWindow(int sessionId) : wxFrame(nullptr, wxID_ANY, "Timber Desktop", wxPoint(30, 30), wxSize(950, 570))
 {
 	this->SetBackgroundColour(wxColor(*wxWHITE));
+	this->sessionId = sessionId;
+	sendMsgBtn = new wxButton(this, 10001, "Send Message", wxPoint(10, 490), wxSize(100, 25));
+	messageTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 430), wxSize(710, 50));
+	messageLb = new wxListBox(this, wxID_ANY, wxPoint(10, 10), wxSize(710, 410));
 }
 
 MainWindow::~MainWindow()
