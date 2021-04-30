7973b0ce7bf19922ba2bd01d704eb5114e133a84
Szalóki Dávid <Szunyix@gmail.com>
2021-04-30 14:39:23 +0200

    Style ( Adds ) :  third element Adds Footer

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -1,23 +1,27 @@
 <footer class="footer">
-	<div class="footer-left">
+	<div class="footer-left"> <!-- first element -->
 		<img src="/assets/vendor/images/large.png">	<!-- picture -->	
 	</div>
 	<ul class="footer-right">
-		<li> <!-- first element -->
+		<li>  <!-- Second element -->
 			<h2>Need Help?</h2>
 			<ul class="box">
-				<li><a href="/Howtoregister">How to register</a></li>				
-			</ul>
-		</li>
-		<li class="features"> <!-- Second element -->
-			<h2>More</h2>
-			<ul class="box">
+				<li><a href="/Howtoregister">How to register</a></li>	
 				<li><a href="#">Brouchures</a></li>
 				<li><a href="#">Donate</a></li>
 				<li><a href="#">Grovernace</a></li>
 				<li><a href="#">Impact Report</a></li>				
 			</ul>
+		</li> <!-- third element -->
+		<li class="features"> 
+			<h2>Follow Us</h2>
+			<ul class="box">
+				<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			</ul>
 		</li>
+
 	</ul>
 
 </footer>
