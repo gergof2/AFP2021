f77e6f9c56e6d67a8d16edc57fda2daa442d2b85
Szalóki Dávid <Szunyix@gmail.com>
2021-04-30 14:34:52 +0200

    Style (Adds) : Second elment adds Footer

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -3,10 +3,19 @@
 		<img src="/assets/vendor/images/large.png">	<!-- picture -->	
 	</div>
 	<ul class="footer-right">
-		<li>
+		<li> <!-- first element -->
 			<h2>Need Help?</h2>
 			<ul class="box">
 				<li><a href="/Howtoregister">How to register</a></li>				
+			</ul>
+		</li>
+		<li class="features"> <!-- Second element -->
+			<h2>More</h2>
+			<ul class="box">
+				<li><a href="#">Brouchures</a></li>
+				<li><a href="#">Donate</a></li>
+				<li><a href="#">Grovernace</a></li>
+				<li><a href="#">Impact Report</a></li>				
 			</ul>
 		</li>
 	</ul>
