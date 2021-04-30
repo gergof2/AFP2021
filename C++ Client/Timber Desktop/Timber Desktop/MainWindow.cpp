8595c0d54d67ef14307246014403360d61669fb0
seresp <71933391+SeresP99@users.noreply.github.com>
2021-04-30 13:27:44 +0200

    Finished MainWindow Constructor

--- C++ Client/Timber Desktop/Timber Desktop/MainWindow.cpp
+++ C++ Client/Timber Desktop/Timber Desktop/MainWindow.cpp
@@ -1,1 +1,6 @@
 #include "MainWindow.h"
+
+MainWindow::MainWindow(int sessionId) : wxFrame(nullptr, wxID_ANY, "Timber Desktop", wxPoint(30, 30), wxSize(950, 570))
+{
+	this->SetBackgroundColour(wxColor(*wxWHITE));
+}
