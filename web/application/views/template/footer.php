f768fbcfd491fb8fc16b7d28ab2f3cdc9edc8467
Szalóki Dávid <Szunyix@gmail.com>
2021-04-30 14:29:20 +0200

    Style (Create) : First Box and element

--- web/application/views/template/footer.php
+++ web/application/views/template/footer.php
@@ -2,7 +2,14 @@
 	<div class="footer-left">
 		<img src="/assets/vendor/images/large.png">	<!-- picture -->	
 	</div>
-		
+	<ul class="footer-right">
+		<li>
+			<h2>Need Help?</h2>
+			<ul class="box">
+				<li><a href="/Howtoregister">How to register</a></li>				
+			</ul>
+		</li>
+	</ul>
 
 </footer>
 
