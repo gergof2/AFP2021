5479047d1299b55b1f096dc0f4281ae1fe1d60f1
Szalóki Dávid <Szunyix@gmail.com>
2021-05-07 16:36:08 +0200

    Backand ( Create ) : Dark theme variable storage on server

--- web/application/controllers/home.php
+++ web/application/controllers/home.php
@@ -3,41 +3,52 @@
 class Home extends Controller {
 
     public $model;
+   
 
     public function __construct()
     {
         parent::__construct();
         $this->model = $this->load_model('api_model');
+
+
     }
     ///////////////////
     //nav link
     # http://localhost/
     public function index()
+    {           
+        $this->load_view('home/index');
+    }
+    public function dataTheme()
     {
-
-        $data['messages'] = ['asd'];
-
-        $this->load_view('home/index', $data);
+        $_SESSION['data-theme'] = $_GET['theme'];
+        header("Refresh:0");      
     }
 
     public function login(){
+        
         $this->load_view('home/login');
     }
 
     public function logout() {
+        $data['data-theme']= $this->data;
         session_destroy();
         $this->redirect('/');
     }
 
     public function Howtoregister(){
+        
         $this->load_view('/helper/Howtoregister');
     }
     public function registration(){
+        
         $this->load_view('home/registration');
     }
 
     public function message() {
-        $data = $this->model->getMessages();
-        $this->load_view('home/message', $data);
+        $data['data-theme']= $_SESSION['data-theme'];
+        $datamessage = $this->model->getMessages();
+
+        $this->load_viewmessage('home/message', $data , $datamessage);
     }
 }
