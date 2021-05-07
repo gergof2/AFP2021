381a1e6fab527466e0c618e8f94a04265ec3ad15
Szalóki Dávid <Szunyix@gmail.com>
2021-05-07 16:48:07 +0200

    Style (Create) : Dark theme For Navbar

--- web/application/views/template/header.php
+++ web/application/views/template/header.php
@@ -21,11 +21,11 @@
 </head>
 <body id="body" data-theme="<?=$_SESSION['data-theme']?>" >
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #CAD5E2";>
+    <nav id="nav" data-theme="<?=$_SESSION['data-theme']?>" class="navbar navbar-expand-lg navbar-light transition">
 
       <div class="container-fluid">
 
-        <a class="navbar-brand mb-0 h1" style="color: #6A1B4D">Timber</a>
+        <a class="navbar-brand mb-0 h1" id="color" data-theme="<?=$_SESSION['data-theme']?>">Timber</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,24 +33,24 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index" style="color: #6A1B4D">Home</a>
+                  <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>" aria-current="page" href="index">Home</a>      
               </li>
               <?php if (isset($_SESSION['username'])): ?>
                   <li class="nav-item">
-                      <a class="nav-link" href="/message" style="color: #6A1B4D">Messages</a>
+                      <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>"  href="/message" >Messages</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="/profile" style=" color: #6A1B4D">My profile: <?=$_SESSION['username']?></a>                      
+                      <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>" href="/profile" >My profile: <?=$_SESSION['username']?></a>                      
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="/logout" style="color: #6A1B4D">Logout</a>
+                      <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>" href="/logout">Logout</a>
                   </li>
               <?php else: ?>
                   <li class="nav-item">
-                      <a class="nav-link" href="/registration" style="color: #6A1B4D">Registration</a>
+                      <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>" href="/registration">Registration</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="/login" style="color: #6A1B4D">Login</a>
+                      <a class="nav-link" id="color" data-theme="<?=$_SESSION['data-theme']?>" href="/login">Login</a>
                   </li>
               <?php endif ?>
           </ul>
