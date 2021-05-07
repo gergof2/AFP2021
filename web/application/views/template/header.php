97a0849215430a434507b52bc2cdea058da3cf61
Szalóki Dávid <Szunyix@gmail.com>
2021-05-07 16:33:58 +0200

    Style (Create) : introduction Dark theme variable

--- web/application/views/template/header.php
+++ web/application/views/template/header.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<?php if (empty($_SESSION['data-theme'])) {
+  $_SESSION['data-theme'] ='light';
+}  ?>
+<html lang="en" data-theme="<?=$_SESSION['data-theme']?>">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
