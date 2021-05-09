60bfa109d91adbf99e3239ae3c7cad9ad6516f41
seresp <71933391+SeresP99@users.noreply.github.com>
2021-05-09 20:06:03 +0200

    Client (Create): Functions Expected to Be Needed In RegisterWindow.h

--- C++ Client/Timber Desktop/Timber Desktop/RegisterWindow.h
+++ C++ Client/Timber Desktop/Timber Desktop/RegisterWindow.h
@@ -14,5 +14,9 @@ public:
 	wxTextCtrl* emailTb = nullptr;
 	wxTextCtrl* usernameTb = nullptr;
 	wxTextCtrl* passwordTb = nullptr;
+
+	void registerButtonClick(wxCommandEvent& evt);
+	bool registerApiCall(string email, string username, string password);
+	wxDECLARE_EVENT_TABLE();
 };
 
