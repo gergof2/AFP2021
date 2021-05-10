25168a924ce4a682e9a4af8b43de81b42ab16007
seresp <71933391+SeresP99@users.noreply.github.com>
2021-05-10 10:46:44 +0200

    Client (Create): RegisterWindow.cpp Constructor and Includes

---
+++ C++ Client/Timber Desktop/Timber Desktop/RegisterWindow.cpp
@@ -0,0 +1,13 @@
+#include "RegisterWindow.h"
+#include "cpr/cpr.h"
+#include "nlohmann/json.hpp"
+using namespace nlohmann;
+RegisterWindow::RegisterWindow() : wxFrame(nullptr, wxID_ANY, "Create Account", wxPoint(10, 10), wxSize(400, 310))
+{
+	this->SetBackgroundColour(wxColor(*wxWHITE));
+	emailTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 20), wxSize(200, 25));
+	usernameTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 50), wxSize(200, 25));
+	passwordTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 100), wxSize(200, 25));
+	registerButton = new wxButton(this, 10001, "", wxPoint(45, 200), wxSize(200, 25));
+}
+
