9ff428acb2310ec52282b58cfee3c688112f052f
seresp <71933391+SeresP99@users.noreply.github.com>
2021-05-13 02:19:14 +0200

    Client (Create): RegisterWindow Static Size

--- C++ Client/Timber Desktop/Timber Desktop/RegisterWindow.cpp
+++ C++ Client/Timber Desktop/Timber Desktop/RegisterWindow.cpp
@@ -10,10 +10,9 @@ wxEND_EVENT_TABLE()
 RegisterWindow::RegisterWindow() : wxFrame(nullptr, wxID_ANY, "Create Account", wxPoint(10, 10), wxSize(390, 210))
 {
 	this->SetBackgroundColour(wxColor(*wxWHITE));
-	emailTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 20), wxSize(200, 25));
-	usernameTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 50), wxSize(200, 25));
-	passwordTb = new wxTextCtrl(this, wxID_ANY, "", wxPoint(45, 100), wxSize(200, 25));
-	registerButton = new wxButton(this, 10001, "", wxPoint(45, 200), wxSize(200, 25));
+
+	this->SetMinSize(wxSize(950, 570));
+	this->SetMaxSize(wxSize(950, 570));
 
 	emailLabel = new wxStaticText(this, wxID_ANY, "Email:", wxPoint(45, 25), wxSize(60, 25), 0, wxStaticTextNameStr);
 	usernameLabel = new wxStaticText(this, wxID_ANY, "Username:", wxPoint(45, 55), wxSize(60, 25), 0, wxStaticTextNameStr);
